b9ec2dfb0d20db71db4f0ca3bdf91defae01aa1b
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2024-10-26 05:19:56 +0200

    Dodano licznik z użyciem i18nPlural

--- src/app/count-students.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountStudentsPipe } from './count-students.pipe';
-
-describe('CountStudentsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountStudentsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
